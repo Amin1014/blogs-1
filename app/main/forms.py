845597dd110e8084578bd30a149548b41297b340
Amin1014 <aminabdi570@gmail.com>
2021-08-22 09:01:10 +0300

    adding queries

---
+++ app/main/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, ValidationError
+from wtforms.validators import Required,Email
+from ..models import User
+from flask_login import current_user
+from flask_wtf.file import FileField
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write a brief bio about you.',validators = [Required()])
+    submit = SubmitField('Save')
+    username = StringField('Input your username',validators=[Required()])
+    email = StringField('Email', validators=[Required(),Email()])
+    profile_pic = FileField('profile pic', validators=[Required()]) 
+
+
+class CreateBlog(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    content = TextAreaField('Blog Content', validators=[Required()])
+    submit = SubmitField('Post')
+    
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            if User.query.filter_by(email = email.data).first():
+                raise ValidationError("This Email has been taken")
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            if User.query.filter_by(username = username.data).first():
+                raise ValidationError("This username has been taken")
